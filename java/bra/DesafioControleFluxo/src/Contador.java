ece87def000b0078b040790e46f398b9a7225824
reinaldo <reinaldo58972@gmail.com>
2025-02-21 16:41:29 -0300

    uml-poo

--- java/bra/DesafioControleFluxo/src/Contador.java
+++ java/bra/DesafioControleFluxo/src/Contador.java
@@ -1,26 +1,40 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) throws Exception {
         Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextInt();
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextInt();
 		
 		try {
-			//chamando o método contendo a lógica de contagem
+			int parametroUm = lerParametro(terminal, "Digite o primeiro parâmetro:");
+            int parametroDois = lerParametro(terminal, "Digite o segundo parâmetro:");
+			
 			contar(parametroUm, parametroDois);
 		
 		}catch (ParametrosInvalidosException exception) {
-			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
-		}
-		terminal.close();
+			System.out.println(exception.getMessage());
+		} 
+            terminal.close();
+			System.out.println("Fim");
 	}
+
+	private static int lerParametro(Scanner terminal, String mensagem) {
+        while (true) {
+            try {
+                System.out.println(mensagem);
+                return terminal.nextInt();
+            } catch (InputMismatchException exception) {
+                System.out.println("Entrada inválida. Precisa ser um número inteiro.");
+                //limpar o buffer
+				terminal.next();
+            }
+        }
+    }
+
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
 		if(parametroUm > parametroDois)
-			throw new ParametrosInvalidosException();
+			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
 		int contagem = parametroDois - parametroUm;
 		for(int i = 1; i <= contagem; i++){
 			System.out.println("Imprimindo o número " + i);
--- java/bra/DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ java/bra/DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,1 +1,5 @@
-public class ParametrosInvalidosException extends Exception{}
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(String mensagem){
+        super(mensagem);
+    }
+}
